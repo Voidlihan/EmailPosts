f3da265e04395a16bd1ef43122e01ea8035d4575
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-01-14 21:17:48 +0600

    Add project files.

---
+++ EmailWithFTP/PostList.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmailWithFTP
+{
+    /// <summary>
+    /// Interaction logic for PostList.xaml
+    /// </summary>
+    public partial class PostList : Window
+    {
+        public PostList()
+        {
+            InitializeComponent();
+        }
+
+        private void SendNewPost(object sender, RoutedEventArgs e)
+        {
+            SendPost sendPost = new SendPost();
+            sendPost.ShowDialog();
+        }
+    }
+}
